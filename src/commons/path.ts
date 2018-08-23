9c9e976f9f58090daeab0690fb417c43dda3ac29
Gustavo Cassel <guscassel@gmail.com>
2018-08-23 13:55:55 -0300

    Added File.ts and Path.ts which are required for Editor.ts corrent operation

---
+++ src/commons/path.ts
@@ -0,0 +1,55 @@
+'use babel';
+
+export default class Path {
+  path: string;
+  
+  constructor(path: any) {
+    if (typeof path === "string") {
+      this.path = path;
+    } else {
+      this.path = path.toString();
+    }
+    this.path = this.path.replace(/\//g, "\\");
+  }
+
+  /**
+   * Retorna o diretório com barra no final
+   */
+  directory() {
+    if (this.path.endsWith("\\")) {
+      return this.path;
+    } else {
+      return this.path.substring(0, this.path.lastIndexOf('\\') + 1);
+    }
+  }
+
+  fileName() {
+    return this.path.substring(this.path.lastIndexOf('\\') + 1, this.path.length);
+  }
+
+  baseName() {
+    var fileName = this.fileName();
+    return fileName.substring(0, fileName.lastIndexOf('.'));
+  }
+
+  extension() {
+    var fileName = this.fileName();
+    return fileName.substring(fileName.lastIndexOf('.'));
+  }
+
+  fullPath() {
+    return this.path;
+  }
+
+  /**
+   * Retorna um novo Path com um nome diferente
+   */
+  setFileName(fileName: string) {
+    return new Path(this.directory() + fileName);
+  }
+
+  toString() {
+    return this.path;
+  }
+
+}
