24a312fe720a9e9061fb2ca94edd7887c43e1fda
Vanduir <vanduir@rech.com.br>
2018-09-11 14:12:42 -0300

    Realizada integração dos pacotes rech-editor-internal e rech-editor-vscode

---
+++ rech-editor-vscode.d.ts
@@ -0,0 +1,150 @@
+/*---------------------------------------------------------------------------------------------
+ * CopyRight Rech Informática Ltda. Todos os direitos reservados.
+ *--------------------------------------------------------------------------------------------*/
+
+declare module 'rech-editor-vscode' {
+
+	/**
+	 * Class to manipulate vscode editor
+	 */
+	export default class Editor {
+
+		/*
+		 * Alguns métodos estão comentados pois utilizam entradas ou saídas do pacote vscode.
+		 * Será necessário criar objetos próprios para encapsular os atributos desejados.
+		 */
+
+		/**
+		 * Construtor do Editor
+		 */
+		constructor();
+	    /**
+	     * Return file path
+	     */
+		getPath(): string;
+	    /**
+	    * Return the document text
+	    */
+		getEditorBuffer(): string;
+	    /**
+	     * Return the text selected
+	     */
+		getSelectionBuffer(): string[];
+	    /**
+	     * Return range of selection
+	     */
+		//getSelectionRange(): Range[];
+	    /**
+	    * Define a editor selection
+	    */
+		//setSelectionRange(range: Range): void;
+	    /**
+	    * Define multiple editor selections
+	    */
+		//setSelectionRanges(range: Range[]): void;
+	    /**
+	    * Return the text of a range
+	    */
+		//getRangeBuffer(range: Range): string;
+	    /**
+	     * Change text of selection
+	     */
+		replaceSelection(buffer: string): void;
+	    /**
+	     * Adjust selection to select the whole line
+	     */
+		selectWholeLines(): void;
+	    /**
+	     * Return current word
+	     */
+		getCurrentWord(): string;
+	    /**
+	     * Select the current word
+	     */
+		selectCurrentWord(): void;
+	    /**
+	     * Return current line
+	     */
+	    getCurrentRow(): number;
+	    /**
+	     * Replace current line with a string
+	     */
+	    setCurrentLine(text: String): void;
+	    /**
+	    * Return length of current line
+	    */
+	    getCurrentLineSize(): number;
+	    /**
+	     * Return current line text
+	     */
+	    getCurrentLine(): string;
+	    /**
+	     * Return the text of current line
+	     */
+	    getLine(lineIndex: number): string;
+	    /**
+	     * Define the cursor position
+	     */
+	    //setCursor(cursor: Position): void;
+	    /**
+	     * Return the cursor position
+	     */
+	    //getCursor(): Position;
+	    /**
+	     * Set the cursor to a column
+	     * OBS: works with multiple cursors
+	     */
+	    setColumn(column: number): void;
+	    /**
+	     * Insert text in a position
+	     */
+	 	//setTextPosition(Position: Position, text: string): void;
+	    /**
+	    * Return if this file is a bat file
+	    */
+	    isBat(): boolean;
+	    /**
+	     * Return if this file is a ruby file
+	     */
+	    isRuby(): boolean;
+	    /**
+	     * Position the cursor on column, type a text in editor and go to specified column
+	     *
+	     * @param text Text to insert in editor
+	     * @param endcolumn Cursor position after the text insertion
+	     * @param startcolumn Cursor position before the text insertion
+	     */
+	    type(text: string, endcolumn?: number, startcolumn?: number): void;
+	    /**
+	     * Calculate the position to column after a text insertion
+	     */
+	    gotoCol(coluna: number): number;
+	    /**
+	     * Insert a text in current selection
+	     * OBS: works with multiple cursors
+	     */
+	    insertText(text: string): void;
+	    /**
+	     * Move down the cursor
+	     */
+	    moveDown(): void;
+	    /**
+	     * Move up the cursor
+	     */
+	    moveUp(): void;
+	    /**
+	     * Move the cursor up/down n times
+	     */
+	    move(num: number): void;
+	    /**
+	     * Opens dialog for file selection
+	     */
+	    //showOpenDialog(defaultDir?: string): Thenable<Uri[] | undefined>;
+	    /**
+	     * Show a information message
+	     */
+	    showInformationMessage(message: string): void;
+	}
+	export { Editor };
+
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,72 +1,41 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, commands } from 'vscode';
-import GeradorCobol from './editor/gerador-cobol';
+import * as vscode from 'vscode';
+import  GeradorCobol  from './editor/gerador-cobol';
 import FonGrep from './fongrep/fongrep';
-import * as TasksProvider from './tasks/tasks-provider';
-import Editor from './editor/editor'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
-    // Register tasks provider
-    TasksProvider.activate(context);
+export function activate(context: vscode.ExtensionContext) {
+
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "rech-test-vscode" is now active!');
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    context.subscriptions.push(commands.registerCommand('extension.cobolMove', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.cobolMove', () => {
         // The code you place here will be executed every time your command is executed
         var gerador = new GeradorCobol();
         gerador.move();
     }));
-    context.subscriptions.push(commands.registerCommand('extension.cobolTo', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.cobolTo', () => {
         // The code you place here will be executed every time your command is executed
         var gerador = new GeradorCobol();
         gerador.to();
     }));
-    context.subscriptions.push(commands.registerCommand('extension.copyLine', () => {
-        // The code you place here will be executed every time your command is executed
-        var gerador = new GeradorCobol();
-        gerador.copyLine();
-    }));
-    context.subscriptions.push(commands.registerCommand('extension.pasteLine', () => {
-        // The code you place here will be executed every time your command is executed
-        var gerador = new GeradorCobol();
-        gerador.pasteLine();
-        
-    }));
-    context.subscriptions.push(commands.registerCommand('extension.fonGrep', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.fonGrep', () => {
         // The code you place here will be executed every time your command is executed
         var fongrep = new FonGrep();
         fongrep.fonGrep();
     }));
-    context.subscriptions.push(commands.registerCommand('extension.openFontesTrunk', () => {
-        // TODO: this command must be transferred to rech-editor-internal
-        // The code you place here will be executed every time your command is executed
-        var editor = new Editor();
-        editor.showOpenDialog(
-            'F:\\FONTES\\',
-            (currentFile) => { editor.openFile(currentFile) },
-        );
-    }));
-    context.subscriptions.push(commands.registerCommand('extension.openScripts', () => {
-        // TODO: this command must be transferred to rech-editor-internal
-        // The code you place here will be executed every time your command is executed
-        var editor = new Editor();
-        editor.showOpenDialog(
-            'F:\\BAT\\',
-            (currentFile) => { editor.openFile(currentFile) },
-        );
-    }));
 
 }
 
+
 // this method is called when your extension is deactivated
 export function deactivate() {
-    TasksProvider.deactivate;
 }
 
 export * from "./editor/editor";
