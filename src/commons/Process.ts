95748241c1ce957f5c4e1163871e76daf7b3b3a4
Gustavo Cassel <guscassel@gmail.com>
2018-09-27 17:13:22 -0300

    Created optional callback to Executor class with general process informations

---
+++ src/commons/Process.ts
@@ -0,0 +1,46 @@
+/**
+ * Class representing a process and it's general information
+ */
+export default class Process {
+
+    /** Standard output */
+    private stdout: string;
+    /** Error output */
+    private stderr: string;
+    /** Error possibly thrown during process execution */
+    private err: Error | null;
+
+    /**
+     * Creates a new process
+     * 
+     * @param stdout 
+     * @param stderr 
+     */
+    constructor(stdout: string, stderr: string, err: Error | null) {
+        this.stdout = stdout;
+        this.stderr = stderr;
+        this.err = err;
+    }
+
+    /**
+     * Returns the standard output
+     */
+    getStdout() {
+        return this.stdout;
+    }
+
+    /**
+     * Returns the error output
+     */
+    getStderr() {
+        return this.stderr;
+    }
+    
+    /**
+     * Returns the error possibly thrown during process execution
+     */
+    getErr() {
+        return this.err;
+    }
+
+}
