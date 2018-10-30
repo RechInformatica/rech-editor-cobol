4918e9e68509b75267cc75625d117f12ed83ddd2
Vanduir <vanduir@rech.com.br>
2018-10-30 08:43:14 -0300

    Aadjusted unit test of sacan

--- src/test/commons/scan.test.ts
+++ src/test/commons/scan.test.ts
@@ -6,21 +6,18 @@ import { File } from "../../commons/file";
 import { Path } from "../../commons/path";
 
 describe("Buffer scan functions", () => {
-  
   // Test scan the buffer
-  it("Scan the buffer", (done) => {
-    new File(new Path(path.resolve(__dirname)+"/../TestFiles/SRICAD.CBL").fullPath()).loadBuffer("latin1").then(buffer => {
+  it("Scan the buffer", done => {
+    new File(new Path(path.resolve(__dirname) + "/../TestFiles/SRICAD.CBL").fullPath()).loadBuffer("latin1").then(buffer => {
       let term = "PLIS-ACESEL";
-      new Scan(buffer.toString()).scan( new RegExp(term, "gi"), (iterator: any) => {
-          if (iterator.lineContent.includes(term)) {
-            assert.equal(34, iterator.column);
-            assert.equal(51720, iterator.row);
-            iterator.stop();
-          }
+      new Scan(buffer.toString()).scan(new RegExp(term, "gi"), (iterator: any) => {
+        if (iterator.lineContent.includes(term)) {
+          assert.equal(34, iterator.column);
+          assert.equal(1721, iterator.row + 1);
+          iterator.stop();
         }
-      );
+      });
       done();
     });
   });
-
 });
