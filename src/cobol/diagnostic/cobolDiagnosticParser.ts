6aeed9b601daffacd7d609e9e96bafc4d5b6a945
Vanduir <vanduir@rech.com.br>
2018-12-04 17:40:26 -0200

    Adjustments in the preprocessing and diagnostic routine

--- src/cobol/diagnostic/cobolDiagnosticParser.ts
+++ src/cobol/diagnostic/cobolDiagnosticParser.ts
@@ -1,9 +1,10 @@
 "use babel";
-import {
-  Diagnostic,
-  DiagnosticSeverity,
-} from "vscode-languageserver";
+import { Diagnostic, DiagnosticSeverity } from "vscode-languageserver";
 import { CobolDiagnostic } from "./cobolDiagnostic";
+import { TextRange } from "./textRange";
+import { TextPosition } from "./textPosition";
+import { File } from "../../commons/file";
+import { Path } from "../../commons/path";
 
 /**
  * Class conteiner of diagnostcs of cobol language
@@ -12,52 +13,50 @@ export class CobolDiagnosticParser {
 
   /**
    * Parses the preprocessor results
-   * 
-   * @param preprocResult 
-   * @param fileName 
+   *
+   * @param preprocResult
+   * @param fileName
    */
-  parser(preprocResult: string, fileName: string): CobolDiagnostic {
-    return this.extractDiagnostic(preprocResult, fileName);
+  parser(preprocResult: string, fileName: string, externalDiagnosticFilter?: (diagnosticMessage: string) => Thenable<boolean>): Promise<CobolDiagnostic> {
+    return this.extractDiagnostic(preprocResult, fileName, externalDiagnosticFilter);
   }
 
   /**
    * Extract the diagnostic in the result of preproc
-   * 
-   * @param preprocResult 
-   * @param fileName 
+   *
+   * @param preprocResult
+   * @param fileName
    */
-  private extractDiagnostic(preprocResult: string, fileName: string): CobolDiagnostic {
+  private extractDiagnostic(preprocResult: string, fileName: string, externalDiagnosticFilter?: (diagnosticMessage: string) => Thenable<boolean> ): Promise<CobolDiagnostic> {
+    return new Promise((resolve, reject) => {
+      let interpreters: Array<Promise<Diagnostic>> = [];
+      let lines = preprocResult.split("\n");
+      let pattern = /\*\*\*\sWarning:\s(.*);\sfile\s=\s([A-Za-z0-9.]+),\sline\s=\s(\d+)\s?(\(Erro\))?/;
+      lines.forEach(currentLine => {
+        interpreters.push(this.interpretsTheErrorMessage(fileName, pattern, currentLine, externalDiagnosticFilter));
+      });
+      Promise.all(interpreters).then((diagnostics) => {
+        resolve(this.buildCobolDiagnostic(diagnostics));
+      }).catch(() => {
+        reject();
+      })
+    });
+  }
+
+  /**
+   * Returns a CobolDiagnostic with diagnostics found
+   * 
+   * @param diagnostics 
+   */
+  private buildCobolDiagnostic(diagnostics: Diagnostic[]): CobolDiagnostic {
     let errors: Diagnostic[] = [];
     let warnings: Diagnostic[] = [];
-    let lines = preprocResult.split("\n");
-    lines.forEach(currentLine => {
-      let pattern = /\*\*\*\sWarning:\s(.*);\sfile\s=\s([A-Za-z0-9.]+),\sline\s=\s(\d+)\s?(\(Erro\))?/;
-      let match = pattern.exec(currentLine);
-      if (match) {
-        if (this.isDiagnosticValid(fileName, match[2], match[1])) {
-          // If the error is in the current source  
-          if (match[4]) {
-            errors.push(
-              this.createDiagnostic(
-                DiagnosticSeverity.Error,
-                new TextRange(
-                  new TextPosition(Number.parseInt(match[3]) - 1, 1),
-                  new TextPosition(Number.parseInt(match[3]) - 1, 120)
-                ),
-                match[1],
-                match[2]
-              )
-            );
-          } else {
-            warnings.push(this.createDiagnostic(DiagnosticSeverity.Warning,
-              new TextRange(
-                new TextPosition(Number.parseInt(match[3]) - 1, 1),
-                new TextPosition(Number.parseInt(match[3]) - 1, 120)
-              ),
-              match[1],
-              match[2]
-            ));
-          }
+    diagnostics.forEach((diagnostic) => {
+      if (diagnostic) {
+        if (diagnostic.severity == DiagnosticSeverity.Error) {
+          errors.push(diagnostic);
+        } else {
+          warnings.push(diagnostic);
         }
       }
     });
@@ -65,15 +64,78 @@ export class CobolDiagnosticParser {
   }
 
   /**
+   * Interpret the error message and insert the diagnostic
+   *
+   * @param fileName
+   * @param pattern
+   * @param currentLine
+   * @param externalDiagnosticFilter
+   */
+  private interpretsTheErrorMessage(fileName: string, pattern: RegExp, currentLine: string, externalDiagnosticFilter?: (diagnosticMessage: string) => Thenable<boolean>): Promise<Diagnostic> {
+    return new Promise((resolve) => {
+      let match = pattern.exec(currentLine);
+      if (!match) {
+        resolve();
+        return;
+      }
+      let fullError, message: any, file: any, line: any, error: any;
+      [fullError, message, file, line, error] = match;
+      if (externalDiagnosticFilter) {
+        externalDiagnosticFilter(message).then(result => {
+          if (result) {
+            this.buildDiagnosticOfError(fileName, message, file, line, error);
+            resolve(this.buildDiagnosticOfError(fileName, message, file, line, error));
+          } else {
+            resolve();
+          }
+        });
+      } else {
+        resolve(this.buildDiagnosticOfError(fileName, message, file, line, error));
+      }
+    });
+  }
+
+  /**
+   * Build the diagnostic of error and insert the result in the {@code errors} or {@code warnings} array
+   *
+   * @param fileName
+   * @param message
+   * @param file
+   * @param line
+   * @param error
+   */
+  private buildDiagnosticOfError(fileName: string, message: string, file: string, line: string, error: boolean) {
+    let nLine = Number.parseInt(line) - 1
+    let diagnosticSeverity: DiagnosticSeverity;
+    if (error) {
+      diagnosticSeverity = DiagnosticSeverity.Error;
+    } else {
+      diagnosticSeverity = DiagnosticSeverity.Warning;
+    }
+    let diagnostic = this.createDiagnostic(
+      fileName,
+      diagnosticSeverity,
+      new TextRange(
+        new TextPosition(nLine, 1),
+        new TextPosition(nLine, 120)
+      ),
+      message,
+      file
+    )
+    return diagnostic;
+  }
+
+  /**
    * Create a diagnostic
    *
+   * @param fileName
    * @param severity
    * @param range
    * @param message
    * @param source
    */
-  private createDiagnostic(severity: DiagnosticSeverity, range: TextRange, message: string, source: string): Diagnostic {
-    
+  private createDiagnostic(fileName: string, severity: DiagnosticSeverity, range: TextRange, message: string, source: string): Diagnostic {
+    let fullFileName = this.fullFileName(new Path(fileName).fullPathWin(), source);
     let diagnostic: Diagnostic = {
       severity: severity,
       range: {
@@ -87,124 +149,29 @@ export class CobolDiagnosticParser {
         }
       },
       message: message,
-      source: source
+      source: fullFileName
     };
+    diagnostic.relatedInformation = [
+      {
+          location: {
+              uri: new Path(fullFileName).fullPathVscode(),
+              range: Object.assign({}, diagnostic.range)
+          },
+          message: message
+      },
+  ];
+
+
     return diagnostic;
   }
 
-  /**
-   * Return if the diagnostic is valided
-   */
-  private isDiagnosticValid(fileName: string, source: string, message: string): boolean {
-    // Is invalid if found the error in other file
-    if (source != fileName) {
-      return false;
+  private fullFileName(fileName: string, source: string): string {
+    let file = new File(new Path(fileName).directory() + source);
+    if (file.exists()) {
+      return file.fileName;
+    } else {
+      return "F:\\Fontes\\" + source;
     }
-    // Not show warning of blank line out of working copy
-    if (message.trim().startsWith("[W003]")) {
-      return false;
-    }
-    // Not show warning of 120 character exceeded
-    if (message.trim().startsWith("[W002]")) {
-      return false;
-    }
-    return true;
   }
+  
 }
-
-/**
- * Position in text
- */
-class TextPosition {
-  /** Line */
-  private _line: number;
-  /** Character */
-  private _character: number;
-
-  constructor(line: number, character: number) {
-    this._line = line;
-    this._character = character;
-  }
-
-  /**
-   * Return the line
-   */
-  public get line() {
-    return this._line;
-  }
-
-  /**
-   * Return the character
-   */
-  public get character() {
-    return this._character;
-  }
-}
-
-/**
- * Position in text
- */
-class TextRange {
-  /** Start position of range */
-  private _start: TextPosition;
-  /** End position of range */
-  private _end: TextPosition;
-
-  constructor(start: TextPosition, end: TextPosition) {
-    this._start = start;
-    this._end = end;
-  }
-
-  /**
-   * Return the start position of range
-   */
-  public get start() {
-    return this._start;
-  }
-
-  /**
-   * Return the end position of range
-   */
-  public get end() {
-    return this._end;
-  }
-}
-
-// The validator creates diagnostics for all uppercase words length 2 and more
-// let text = textDocument.getText();
-// let pattern = /\b[A-Z]{2,}\b/g;
-// let m: RegExpExecArray | null;
-
-// let problems = 0;
-// let diagnostics: Diagnostic[] = [];
-// while ((m = pattern.exec(text)) && problems < 1) {
-//     problems++;
-//     let diagnosic: Diagnostic = {
-//         severity: DiagnosticSeverity.Warning,
-//         range: {
-//             start: textDocument.positionAt(m.index),
-//             end: textDocument.positionAt(m.index + m[0].length)
-//         },
-//         message: `${m[0]} is all uppercase.`,
-//         source: 'ex'
-//     };
-//     if (hasDiagnosticRelatedInformationCapability) {
-//         diagnosic.relatedInformation = [
-//             {
-//                 location: {
-//                     uri: textDocument.uri,
-//                     range: Object.assign({}, diagnosic.range)
-//                 },
-//                 message: 'Spelling matters'
-//             },
-//             {
-//                 location: {
-//                     uri: textDocument.uri,
-//                     range: Object.assign({}, diagnosic.range)
-//                 },
-//                 message: 'Particularly for names'
-//             }
-//         ];
-//     }
-//     diagnostics.push(diagnosic);
-// }
--- src/cobol/diagnostic/diagnostician.ts
+++ src/cobol/diagnostic/diagnostician.ts
@@ -1,73 +1,119 @@
 "use babel";
-import { TextDocument, Diagnostic } from 'vscode-languageserver';
-import { CobolDiagnostic } from './cobolDiagnostic';
-import { Path } from '../../commons/path';
-import { File } from '../../commons/file';
-import { CobolDiagnosticParser } from './cobolDiagnosticParser';
+import { TextDocument, Diagnostic } from "vscode-languageserver";
+import { CobolDiagnostic } from "./cobolDiagnostic";
+import { Path } from "../../commons/path";
+import { File } from "../../commons/file";
+import { CobolDiagnosticParser } from "./cobolDiagnosticParser";
+
+/* Time in millis representing an old file */
+const OLD_FILE_IN_MILLIS: number = 7000;
 
 /**
  * Class to diagnose sources
  */
 export class Diagnostician {
-
-    /**
-     * Diagnoses the source
-     * 
-     * @param textDocument 
-     * @param preprocessorResultFileName 
-     * @param PreprocessCallback 
-     */
-    public diagnose(textDocument: TextDocument, PreprocessCallback: (uri: string) => Thenable<string>): Promise<Diagnostic[]> {
-        return new Promise((resolve, reject) => {
-            this.findErrorsAndWarnings(textDocument, PreprocessCallback).then((cobolDiagnostic) => {
-                this.extractDiagnostics(cobolDiagnostic).then((diagnostics) => {
-                    resolve(diagnostics);
-                }).catch(() => {
-                    reject();
-                });
+  /**
+   * Diagnoses the source
+   *
+   * @param textDocument
+   * @param preprocessorResultFileName
+   * @param PreprocessCallback
+   */
+  public diagnose(
+    textDocument: TextDocument,
+    PreprocessCallback: (uri: string) => Thenable<string>,
+    externalDiagnosticFilter?: (diagnosticMessage: string) => Thenable<boolean>
+  ): Promise<Diagnostic[]> {
+    return new Promise((resolve, reject) => {
+      this.findErrorsAndWarnings(
+        textDocument,
+        PreprocessCallback,
+        externalDiagnosticFilter
+      ).then(cobolDiagnostic => {
+          if (!cobolDiagnostic) {
+            reject();
+            return
+          }
+          this.extractDiagnostics(cobolDiagnostic).then(diagnostics => {
+              resolve(diagnostics);
             }).catch(() => {
-                reject();
+              reject();
             });
+        }).catch(() => {
+          reject();
         });
-    }
+    });
+  }
 
-    /**
-     * Find errors or/and warnings
-     * 
-     * @param textDocument 
-     * @param preprocessorResultFileName 
-     * @param PreprocessCallback 
-     */
-    private findErrorsAndWarnings(textDocument: TextDocument, PreprocessCallback: (uri: string) => Thenable<string>) {
-        return new Promise<CobolDiagnostic>((resolve, reject) => {
-            let tmpFile = new File("C:\\TMP\\" + new Path(textDocument.uri).fileName());
-            tmpFile.saveBuffer([textDocument.getText()]).then( () => {
-                PreprocessCallback(tmpFile.fileName).then((buffer) => {
-                    let fileName = new Path(textDocument.uri).fileName();
-                    resolve(new CobolDiagnosticParser().parser(buffer, fileName));
-                });
-            }).catch(() => {
+  /**
+   * Find errors or/and warnings
+   *
+   * @param textDocument
+   * @param preprocessorResultFileName
+   * @param PreprocessCallback
+   */
+  private findErrorsAndWarnings(
+    textDocument: TextDocument,
+    PreprocessCallback: (uri: string) => Thenable<string>,
+    externalDiagnosticFilter?: (diagnosticMessage: string) => Thenable<boolean>): Promise<CobolDiagnostic | undefined> {
+    return new Promise<CobolDiagnostic>((resolve, reject) => {
+      // If not a cobol processable source
+      if (new Path(textDocument.uri).extension().toUpperCase() != ".CBL") {
+        reject();
+      }
+      let tmpFile = new File(
+        "C:\\TMP\\" + new Path(textDocument.uri).fileName()
+      );
+      if (tmpFile.exists()) {
+        if (this.isFileOld(tmpFile)) {
+          tmpFile.delete();
+        }
+        resolve(undefined);
+        return;
+      }
+      tmpFile.saveBuffer([textDocument.getText()]).then(() => {
+          PreprocessCallback(tmpFile.fileName).then(buffer => {
+            let fileName = new Path(textDocument.uri).fullPath();
+            new CobolDiagnosticParser().parser(buffer, fileName, externalDiagnosticFilter).then(cobolDiagnostic => {
+                tmpFile.delete();
+                resolve(cobolDiagnostic);
+              }).catch(() => {
+                tmpFile.delete();
                 reject();
-            })
+              })
+          });
+        }).catch(() => {
+          reject();
         })
-    }
+    });
+  }
 
-    /**
-     * Returns an array containing the diagnostics
-     * 
-     * @param cobolDiagnostic 
-     */
-    private extractDiagnostics(cobolDiagnostic: CobolDiagnostic): Promise<Diagnostic[]> {
-        return new Promise((resolve) => {
-            let diagnostics: Diagnostic[] = [];
-            cobolDiagnostic.errors.forEach((diagnostic) => {
-                diagnostics.push(diagnostic);
-            });
-            cobolDiagnostic.warnings.forEach((diagnostic) => {
-                diagnostics.push(diagnostic);
-            });
-            resolve(diagnostics);
-        })
-    }
-
+  /**
+   * Return {@code true} if is a old file
+   * 
+   * @param file 
+   */
+  private isFileOld(file: File): boolean {
+    let fileLastModified = file.lastModified().getTime();
+      let currentTime = new Date().getTime()
+      let resultTime = currentTime - fileLastModified;
+      return (resultTime > OLD_FILE_IN_MILLIS)
+  }
+  /**
+   * Returns an array containing the diagnostics
+   *
+   * @param cobolDiagnostic
+   */
+  private extractDiagnostics(cobolDiagnostic: CobolDiagnostic): Promise<Diagnostic[]> {
+    return new Promise(resolve => {
+      let diagnostics: Diagnostic[] = [];
+      cobolDiagnostic.errors.forEach(diagnostic => {
+        diagnostics.push(diagnostic);
+      });
+      cobolDiagnostic.warnings.forEach(diagnostic => {
+        diagnostics.push(diagnostic);
+      });
+      resolve(diagnostics);
+    });
+  }
 }
