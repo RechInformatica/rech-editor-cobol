5d491bdc11ed3c68914b1f0348dac4a90016c649
Felipe <felipeigferreira@gmail.com>
2019-01-14 17:25:24 -0200

    RechDoc decoration refactored to decorate only RechDoc.

--- src/decoration/CustomDecorator.ts
+++ src/decoration/CustomDecorator.ts
@@ -7,12 +7,11 @@ export class CustomDecorator {
     public static activate(context: ExtensionContext) {
         let activeEditor: vscode.TextEditor | undefined;
         let parser: Parser = new Parser();
-    
+
         // Called to handle events below
         let updateDecorations = function () {
             // If active window is open and language is supported
-            if (activeEditor && parser.supportedLanguage) {
-                parser.FindSingleLineComments(activeEditor);
+            if (activeEditor && activeEditor.document.languageId == "COBOL") {
                 parser.FindRechDocComments(activeEditor);
                 parser.ApplyDecorations(activeEditor);
             }
@@ -20,8 +19,6 @@ export class CustomDecorator {
         // Get the active editor for the first time and initialize the regex
         if (vscode.window.activeTextEditor) {
             activeEditor = vscode.window.activeTextEditor;
-            // Set the regex patterns for the specified language's comments
-            parser.SetRegex(activeEditor.document.languageId);
             // Trigger first update of decorators
             triggerUpdateDecorations();
         }
@@ -29,22 +26,19 @@ export class CustomDecorator {
         vscode.window.onDidChangeActiveTextEditor(editor => {
             activeEditor = editor;
             if (editor) {
-                // Set regex for updated language
-                parser.SetRegex(editor.document.languageId);
                 // Trigger update to set decorations for newly active file
                 triggerUpdateDecorations();
             }
         }, null, context.subscriptions);
-    
+
         // Handle called when change text content of active editor
         vscode.workspace.onDidChangeTextDocument(event => {
-    
             // Trigger updates if the text was changed in the same document
             if (activeEditor && event.document === activeEditor.document) {
                 triggerUpdateDecorations();
             }
         }, null, context.subscriptions);
-    
+
         // This timer waits 200ms before updating decorations, avoiding calling update too often
         var timeout: NodeJS.Timer;
         function triggerUpdateDecorations() {
