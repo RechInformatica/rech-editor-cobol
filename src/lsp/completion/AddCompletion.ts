16b68bf231488890ca882c8f83137e71e358952b
Gustavo Cassel <guscassel@gmail.com>
2019-01-26 18:02:09 -0200

    Fixed problem with filling spaces on ADD clause suggestion

--- src/lsp/completion/AddCompletion.ts
+++ src/lsp/completion/AddCompletion.ts
@@ -9,7 +9,9 @@ export class AddCompletion implements CompletionInterface {
 
     public generate(_line: number, column: number, _lines: string[]): Promise<CompletionItem[]> {
         return new Promise((resolve) => {
-            let text = "add" + CompletionUtils.fillMissingSpaces(20, column + 2) + "${0}";
+            let lineWithoutEnter = _lines[_line].replace("\r", "").replace("\n", "");
+            let futureLine = CompletionUtils.replaceLastWord(lineWithoutEnter, "add");
+            let text = "add" + CompletionUtils.fillSpacesFromWordEnd(20, column, futureLine) + "${0}";            
             resolve( [{
                 label: 'ADD command',
                 detail: 'Generates ADD command and sets cursor on the first variable',
