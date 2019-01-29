3f8949d59cfe44a90e8646c3ca4ca094128a5306
Gustavo Cassel <guscassel@gmail.com>
2019-01-29 16:10:56 -0200

    Removed deprecated API 'fillMissingSpaces'

--- src/lsp/commons/CompletionUtils.ts
+++ src/lsp/commons/CompletionUtils.ts
@@ -23,45 +23,6 @@ export class CompletionUtils {
   }
 
   /**
-   * Fills missing spaces between the current cursor column and the target final
-   * column.
-   *
-   * Example:
-   * CompletionUtils.fillMissingSpaces(10, 5) -> generates "    " (four spaces)
-   * CompletionUtils.fillMissingSpaces(5, 1) -> generates "   " (three spaces)
-   *
-   * If the cursor column is equal or greater than the target final column, a single
-   * space is added so the completion will not be attached to previous text in the line.
-   *
-   * Example:
-   * CompletionUtils.fillMissingSpaces(10, 10) -> generates " " (single space)
-   * CompletionUtils.fillMissingSpaces(10, 12) -> generates " " (single space)
-   *
-   * @param targetFinalColumn final column until where spaces should be inserted
-   * @param currentCursorColumn column where cursor is currently positioned
-   * @param currentLineText current line text
-   * @deprecated fillExactMissingSpaces should be used instead
-   */
-  public static fillMissingSpaces(targetFinalColumn: number, currentCursorColumn: number, currentLineText?: string): string {
-    let missingSpaces = targetFinalColumn - currentCursorColumn;
-    let text = "";
-    for (var i = 1; i < missingSpaces; i++) {
-      text = text.concat(" ");
-    }
-    if (text.length === 0) {
-      if (currentLineText) {
-        let lastChar = currentLineText.charAt(currentCursorColumn - 1);
-        if (lastChar !== " ") {
-          text = " ";
-        }
-      } else {
-        text = " ";
-      }
-    }
-    return text;
-  }
-
-  /**
    * Fills missing spaces between the colun where the word found on
    * current cursor starts, and the target final column.
    *
--- src/test/lsp/commons/CompletionUtils.test.ts
+++ src/test/lsp/commons/CompletionUtils.test.ts
@@ -4,27 +4,6 @@ import { CompletionUtils } from '../../../lsp/commons/CompletionUtils';
 
   describe('CompletionUtils functions', () => {
 
-    it('Checks filling missing spaces without line text', () => {
-      expect("    ").to.equal(CompletionUtils.fillMissingSpaces(10, 5));
-      expect("         ").to.equal(CompletionUtils.fillMissingSpaces(15, 5));
-      expect("   ").to.equal(CompletionUtils.fillMissingSpaces(5, 1));
-      expect(" ").to.equal(CompletionUtils.fillMissingSpaces(10, 10));
-      expect(" ").to.equal(CompletionUtils.fillMissingSpaces(10, 12));
-      expect(" ").to.equal(CompletionUtils.fillMissingSpaces(35, 35));
-      expect(" ").to.equal(CompletionUtils.fillMissingSpaces(35, 34));
-    });
-
-    it('Checks filling missing spaces with line text', () => {
-      expect("") .to.equal(CompletionUtils.fillMissingSpaces(35, 34, "           05 w-var               "));
-      expect("") .to.equal(CompletionUtils.fillMissingSpaces(35, 35, "           05 w-var                "));
-      expect("") .to.equal(CompletionUtils.fillMissingSpaces(3, 5, "           05 w-var               "));
-      expect("") .to.equal(CompletionUtils.fillMissingSpaces(3, 3, "           05 w-var               "));
-      expect(" ").to.equal(CompletionUtils.fillMissingSpaces(35, 34, "           05 w-var              L"));
-      expect(" ").to.equal(CompletionUtils.fillMissingSpaces(35, 33, "           05 w-var              "));
-      expect("  ").to.equal(CompletionUtils.fillMissingSpaces(35, 32, "           05 w-var             "));
-      expect("   ").to.equal(CompletionUtils.fillMissingSpaces(35, 31, "           05 w-var            "));
-    });
-
     it('Checks filling spaces from word start', () => {
       expect("                   ").to.equal(CompletionUtils.fillSpacesFromWordStart(35, 15, "                                  "));
       expect("                               ").to.equal(CompletionUtils.fillSpacesFromWordStart(35, 3, "        "));
