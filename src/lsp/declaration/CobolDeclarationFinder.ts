9d502f83f2481e57ca48026680ac5fe3110bee55
Gustavo Cassel <guscassel@gmail.com>
2019-10-29 09:02:19 -0300

    Now regex to esapce COBOL characters is simpler than before

--- src/lsp/declaration/CobolDeclarationFinder.ts
+++ src/lsp/declaration/CobolDeclarationFinder.ts
@@ -426,7 +426,7 @@ export class CobolDeclarationFinder {
       const linesFromText = this.text.split("\n");
       let line = linesFromText[referenceLine].trim();
       // Escape some COBOL characters which are Regex reserved symbols
-      line = line.replace(/([\*\.\,\(\\)\\+\=\>\:])/g, "\\$1");
+      line = line.replace(/([*.,()\\+=>:])/g, "\\$1");
       let referenceLineForScan: number;
       const patter = new RegExp(`${line}\\s*\\*\\>\\:\\s+\\d+\\s+\\d+$`, 'gi');
       new Scan(buffer).scan(patter, (iterator: any) => {
