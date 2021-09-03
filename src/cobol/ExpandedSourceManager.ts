1aab034598ea1feb952a3a3d4c57499ff7969c71
Vanduir <vanduir@rech.com.br>
2021-09-03 17:34:49 -0300

    Created cache invalidation strategy

--- src/cobol/ExpandedSourceManager.ts
+++ src/cobol/ExpandedSourceManager.ts
@@ -2,12 +2,15 @@ import { Path } from "../commons/path";
 import { File } from "../commons/file";
 import { Log } from "../commons/Log";
 
+
 /**
  * Class to manager the expanded source
  */
 export class ExpandedSourceManager {
+  /** Max cache time in ms */
+  private static maxCacheTime = 300;
   /** Expanded sources */
-  private static expandedSource: Map<string, string> = new Map();
+  private static expandedSourceCache: Map<string, {time: number, expandedSource: string}> = new Map();
   /** callback to expander the source */
   private static callbackSourceExpander: ((uri: string, cacheFileName: string) => Thenable<{}>) | undefined;
   /** callback to show SatusBar from SourceExpander */
@@ -16,6 +19,15 @@ export class ExpandedSourceManager {
   private static callbackHideStatusBarFromSourceExpander: (() => void) | undefined;
   /** Source to expand*/
   private source: string;
+
+  /**
+   * Defines the max cache time of the cached source in ms
+   *
+   * @param maxCacheTime max cache time of the cached source in ms
+   */
+  public static setMaxCacheTime(maxCacheTime: number) {
+    ExpandedSourceManager.maxCacheTime = maxCacheTime;
+  }
 
   constructor(source: string) {
     this.source = source;
@@ -42,7 +54,7 @@ export class ExpandedSourceManager {
         Log.get().info("ExpandedSourceManager.callbackSourceExpander was finality");
         const file = new File(ExpandedSourceManager.buildExpandedSourceFileName(this.source));
         file.loadBuffer("latin1").then((buffer) => {
-          ExpandedSourceManager.expandedSource.set(this.source, buffer);
+          ExpandedSourceManager.setExpandedSourceInCache(this.source, buffer);
           Log.get().info("Expanded source loaded successfully");
           if (ExpandedSourceManager.callbackHideStatusBarFromSourceExpander) {
             ExpandedSourceManager.callbackHideStatusBarFromSourceExpander();
@@ -100,7 +112,7 @@ export class ExpandedSourceManager {
   public static getExpandedSource(source: string): Promise<string> {
     Log.get().info("Expanded source requested. Source: " + source);
     return new Promise((resolve, reject) => {
-      const expandedSource = ExpandedSourceManager.expandedSource.get(source);
+      const expandedSource = ExpandedSourceManager.getExpandedSourceFromCache(source);
       if (expandedSource) {
         Log.get().info("Expanded source got from cache. Source: " + source);
         return resolve(expandedSource);
@@ -118,12 +130,35 @@ export class ExpandedSourceManager {
   }
 
   /**
-   * Remove the source of the cache
+   * Returns the expanded source from cache
+   */
+  private static getExpandedSourceFromCache(source: string) {
+    const cache = ExpandedSourceManager.expandedSourceCache.get(source);
+    if (!cache) return undefined;
+    const currentTime = new Date().getTime();
+    if ((currentTime - cache.time) > ExpandedSourceManager.maxCacheTime) {
+      return undefined;
+    }
+    return cache.expandedSource;
+  }
+
+  /**
+   * Set source on cache
+   *
+   * @param source
+   * @param expandedSource
+   */
+  private static setExpandedSourceInCache(source: string, expandedSource: string) {
+    ExpandedSourceManager.expandedSourceCache.set(source, {time: new Date().getTime(), expandedSource: expandedSource});
+  }
+
+  /**
+   * Remove source from cache
    *
    * @param source
    */
   public static removeSourceOfCache(source: string) {
-    ExpandedSourceManager.expandedSource.delete(source);
+    ExpandedSourceManager.expandedSourceCache.delete(source);
   }
 
   /**
