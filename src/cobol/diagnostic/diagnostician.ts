25a4db30f23e600b58ebe3a9818283ccd251dd24
Vanduir <vanduir@rech.com.br>
2021-09-15 17:37:35 -0300

    Solved bug in calling diagnostician with .cbl or .cob file

--- src/cobol/diagnostic/diagnostician.ts
+++ src/cobol/diagnostic/diagnostician.ts
@@ -70,8 +70,8 @@ export class Diagnostician {
       // If not a cobol processable source
       const documentPath = new Path(textDocument.uri);
       Log.get().info("FindErrorsAndWarnings from " + documentPath);
-      if (documentPath.extension().toUpperCase() != ".CBL" || documentPath.extension().toUpperCase() != ".COB") {
-        Log.get().info("Rejected because extension is not a .CBL or .COB");
+      if (documentPath.extension().toUpperCase() != ".CBL" && documentPath.extension().toUpperCase() != ".COB") {
+        Log.get().info("Rejected because extension is not a .CBL or .COB. extension: " + documentPath.extension().toUpperCase());
         return reject();
       }
       const dir = new File(DIAGNOSTIC_ROOT_DIR + require("os").userInfo().username + "\\");
